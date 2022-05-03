5b3cf548d774b41cc430529adc9df4e6d13661ca
Edwin David <40572443+edco29@users.noreply.github.com>
2022-05-03 11:25:40 -0500

    Initial commit

---
+++ api/quote/index.js
@@ -0,0 +1,41 @@
+const https = require('https');
+
+const unplashApi = 'https://source.unsplash.com/1600x900?dream';
+const quotes = [
+  'Wherever you go, no matter what the weather, always bring your own sunshine.',
+  'Tú\ eres asombroso.',
+  'La felicidad es la unica cosa que se multiplica cuando la compartes.',
+  'Siempre parece imposible hasta que esta hecho.',
+  'Deja que tu energia positiva inspire confianza en los demas.',
+  'Lo mejor esta por venir.',
+  'La hora mas oscura es la que antecede al alba',
+  'You\'re capable of more than you can even dream.',
+  'Mereces lo mejor.',
+  'Keep going, you\'re doing well.',
+  'Permanece positivo; permanece con esperanzas'
+];
+
+async function getImage() {
+  return new Promise((resolve, reject) => {
+    https.get(unplashApi, (response) => {
+      // API returns a HTTP 302 code, we only want the final image URL
+      resolve(response.headers.location);
+    }).on('error', (error) => {
+      reject(error.message);
+    });
+  });
+}
+
+module.exports = async function (context, req) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  const image = await getImage();
+  const text = quotes[Math.floor(Math.random() * quotes.length)];
+
+  context.res = {
+    body: {
+      image,
+      text
+    }
+  };
+};
